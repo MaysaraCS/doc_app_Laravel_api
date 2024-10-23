9c7b4c09bfea17b24f61a48dc8ddcbf7187a4c30
Maysara <maysaracs1001@gmail.com>
2024-10-23 16:28:35 +0800

    add new user type as doc

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -26,10 +26,18 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
+        $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
+            'type' => 'doctor',
             'password' => Hash::make($input['password']),
         ]);
-    }
+
+        $doctorInfo = Doctor::create([
+                'doc_id' => $user->id,
+                'status' => 'active'
+            ]);
+
+            return $user;
+        }
 }
