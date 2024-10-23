71640b09e5392ad2e2c0b1802d2d981d0f32dffa
Maysara <maysaracs1001@gmail.com>
2024-10-23 16:21:25 +0800

    data migration fixed

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Doctor extends Model
 {
-    //
+    use HasFactory;
+
+    //these are fillable input
+    protected $fillable = [
+        'doc_id',
+        'category',
+        'patients',
+        'experience',
+        'bio_data',
+        'status',
+    ];
+
+    //state this is belong to user table
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/UserDetails.php
+++ app/Models/UserDetails.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserDetails extends Model
 {
-    //
+    use HasFactory;
+
+    //these are fillable input
+    protected $fillable = [
+        'user_id',
+        'bio_data',
+        'fav',
+        'status',
+    ];
+
+    //state this is belong to user table
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
